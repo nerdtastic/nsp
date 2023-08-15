6da3e763bceec90fdda1007f11da103ff5eb807b
Thomas Pikes <thomaspikes@gmail.com>
2023-08-15 14:32:55 -0400

    f

--- hello.php
+++ hello.php
@@ -21,16 +21,16 @@ if (($handle = fopen("timecard_1_22.csv", "r")) !== FALSE) {
         $stopDate = strtotime($data[2] . $data[3]);
         $firstName = $data[4];
         $lastName = $data[5];
-
+        echo "looking for: " . $firstName . " " . $lastName . "\n";
         if($startDate){
-            // $sql = "SELECT id, firstname, lastname from person where lower(firstname) = lower('" . $firstname . "') and lower(lastname) = lower('" . $lastname . "')";
-            // $result = $conn->query($sql);
-            // echo $result->num_rows . "\n";
+            $sql = "SELECT id from person where lower(firstname) = lower('" . $firstName . "') and lower(lastname) = lower('" . $lastName . "')";
+            $result = $conn->query($sql);
+            echo $result->num_rows . "\n";
             if(!$stopDate){
                 echo "Missing stopDate on row:" . $row . "\n";
             }
         } else {
-            echo "skipping row:" . $row . "\n";
+            //echo "skipping row:" . $row . "\n";
         }
         $row++;
     }
