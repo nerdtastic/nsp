ada1a838dff38ab70db2fc8cab7e43e934b8f66d
Thomas Pikes <thomaspikes@gmail.com>
2023-08-15 16:37:46 -0400

    f

--- hello.php
+++ hello.php
@@ -16,7 +16,7 @@ $row = 1;
 5: lastName
 */
 $files = scandir(".");
-echo $files;
+print_r($files);
 if (($handle = fopen("timecard_1_8.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $startDate = strtotime($data[0] . $data[1]);
