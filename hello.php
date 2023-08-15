1a69b50e49917549e5f8deaa3c2751182a1f7a0f
Thomas Pikes <thomaspikes@gmail.com>
2023-08-15 13:35:26 -0400

    f

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@ if (($handle = fopen("timecard_1_22.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         $row++;
-        $startDate = strtotime($data[0]);
+        $startDate = strtotime($data[0] . $data[1]);
         echo $startDate . "\n";
         for ($c=0; $c < $num; $c++) {
             
