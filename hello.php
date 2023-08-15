01eda15857b401f2487e09e7cf4484400f6ebccc
Thomas Pikes <thomaspikes@gmail.com>
2023-08-15 14:02:47 -0400

    f

--- hello.php
+++ hello.php
@@ -4,13 +4,19 @@ include_once 'connection.php';
 
 $row = 1;
 
+$sql = "SELECT id, firstname, lastname from person";
+$result = $conn->query($sql);
+
+echo $result->num_rows . "\n"
+
+
 if (($handle = fopen("timecard_1_22.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         $row++;
         $startDate = strtotime($data[0] . $data[1]);
         if($startDate){
-            echo date('d/M/Y h:i:s', $startDate) . "\n";
+            //echo date('d/M/Y h:i:s', $startDate) . "\n";
         }
         
         for ($c=0; $c < $num; $c++) {
