9e37a45df76d132b4b57972f88e9b71e8a8c14d8
Thomas Pikes <thomaspikes@gmail.com>
2023-08-15 14:26:44 -0400

    f

--- hello.php
+++ hello.php
@@ -17,19 +17,20 @@ $row = 1;
 */
 if (($handle = fopen("timecard_1_22.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        $startDate = strtotime($data[0] . $data[1]);
-        $stopDate = strtotime($data[2] . $data[3]);
-        $firstName = $data[4];
-        $lastName = $data[5];
-        $sql = "SELECT id, firstname, lastname from person where lower(firstname) = lower('" . $firstname . "') and lower(lastname) = lower('" . $lastname . "')";
-        $result = $conn->query($sql);
-        echo $result->num_rows . "\n";
         if(!$startDate){
-            echo "Missing startDate on row:" . $row . "\n";
-        }
-        if(!$stopDate){
-            echo "Missing endDate on row:" . $row . "\n";
+            $num = count($data);
+            $startDate = strtotime($data[0] . $data[1]);
+            $stopDate = strtotime($data[2] . $data[3]);
+            $firstName = $data[4];
+            $lastName = $data[5];
+            $sql = "SELECT id, firstname, lastname from person where lower(firstname) = lower('" . $firstname . "') and lower(lastname) = lower('" . $lastname . "')";
+            $result = $conn->query($sql);
+            echo $result->num_rows . "\n";
+            if(!$stopDate){
+                echo "Missing stopDate on row:" . $row . "\n";
+            }
+        } else {
+            echo "skipping row:" . $row . "\n";
         }
         $row++;
     }
