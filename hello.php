bb6121f5a24d5fb972851e4035750a9419b9d20a
Thomas Pikes <thomaspikes@gmail.com>
2023-08-15 14:27:49 -0400

    f

--- hello.php
+++ hello.php
@@ -17,12 +17,12 @@ $row = 1;
 */
 if (($handle = fopen("timecard_1_22.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $startDate = strtotime($data[0] . $data[1]);
+        $stopDate = strtotime($data[2] . $data[3]);
+        $firstName = $data[4];
+        $lastName = $data[5];
+        
         if(!$startDate){
-            $num = count($data);
-            $startDate = strtotime($data[0] . $data[1]);
-            $stopDate = strtotime($data[2] . $data[3]);
-            $firstName = $data[4];
-            $lastName = $data[5];
             $sql = "SELECT id, firstname, lastname from person where lower(firstname) = lower('" . $firstname . "') and lower(lastname) = lower('" . $lastname . "')";
             $result = $conn->query($sql);
             echo $result->num_rows . "\n";
