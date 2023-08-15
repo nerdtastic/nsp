61d75d917af6cc985245cbdbc8cb30a25bb3ca7b
Thomas Pikes <thomaspikes@gmail.com>
2023-08-15 13:46:16 -0400

    f

--- hello.php
+++ hello.php
@@ -1,4 +1,14 @@
 <?php
+$servername = "cburg.dreamhost.com";
+$username = "nerdtastic";
+$password = "jL^CT@BCe3TCacpwfhE6NyBY";
+
+$conn = new mysqli($servername, $username, $password);
+if($conn -> connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "connected to the thing";
+
 $row = 1;
 if (($handle = fopen("timecard_1_22.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
