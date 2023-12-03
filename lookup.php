b913a6ebc98ec481fb8307330fadb99fbd961632
Thomas Pikes <thomaspikes@gmail.com>
2023-12-03 17:22:03 -0500

    efefef

--- lookup.php
+++ lookup.php
@@ -15,7 +15,7 @@ foreach($files as $file){
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $firstName = mysqli_real_escape_string($conn, $data[0]);
             $lastName = mysqli_real_escape_string($conn, $data[1]);
-            $sql = "SELECT id, firstname, lastname from person where lower(firstname) = lower('" . $firstName . "') and lower(lastname) = lower('" . $lastName . "')";
+            $sql = "SELECT id, firstname, lastname from person where lower(firstname) = lower('" . $firstName . "') and lower(lastname) = lower('" . $lastName . "') ORDER BY lastname, firstname";
             $result = $conn->query($sql);
             $queryRow = $result->fetch_assoc();
             echo "looking for: " . $queryRow["lastname"] . ", " . $queryRow["firstname"] . "\n";
