5d34efe001d3a875e5381ccb05f4d5fe9b494a4f
Thomas Pikes <thomaspikes@gmail.com>
2023-12-03 17:13:53 -0500

    efefef

--- lookup.php
+++ lookup.php
@@ -15,6 +15,7 @@ foreach($files as $file){
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $firstName = mysqli_real_escape_string($conn, $data[0]);
             $lastName = mysqli_real_escape_string($conn, $data[1]);
+            echo $firstName;
             $sql = "SELECT * FROM shift where person = ( SELECT id from person where lower(firstname) = lower('" . $firstName . "') and lower(lastname) = lower('" . $lastName . "') )";
             $result = $conn->query($sql);
 
@@ -22,16 +23,6 @@ foreach($files as $file){
                 echo print_r($row);       // Print the entire row data
             }
 
-            // if($result->num_rows == 1){
-            //     $queryRow = $result->fetch_assoc();
-            //     if($stopDate){
-            //         $sql = "SELECT * from shift where person = " . $queryRow["id"];
-            //         echo $sql. "\n";
-            //         $conn->query($sql);
-            //     }
-            // } else {
-            //     echo $result->num_rows . " rows for " . $lastName . ", " . $firstName . ". File: " . $file . "\n";
-            // }
             $row++;
         }
         fclose($handle);
